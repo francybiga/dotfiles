68021a87db7932289f1effcf50dc425fd869e590
francybiga <francybiga@gmail.com>
2016-05-04 11:14:31 +0200

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!bin/bash
+#!/bin/bash
 #########################
-#This scripts creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+#This scripts creates symlinks from the home directory to any desired dotfiles in ~/Development/dotfiles
 ########################
 
 #Variables
 
-dir=~/dotfiles
-old_dir=~/dotfiles_old
+dir=~/Development/dotfiles
+old_dir=~/Development/dotfiles_old
 files="vimrc vim"		#list of files and folders to symlink in HOME
 
 ########
@@ -21,10 +21,10 @@ echo -n "Changing to the $dir directory..."
 cd $old_dir
 echo "Done"
 
-#move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+#move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/Development/dotfiles directory specified in $files
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $old_dir"
-	mv ~/.$file ~/old_dir/
+	mv ~/.$file old_dir
 	echo "Creating symlink to $file in home directory"
 	ln -s $dir/$file ~/.$file
 done
