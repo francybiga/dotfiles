87e5e1ccc7b9b724081706fd82afd02ff7367f06
francybiga <francybiga@gmail.com>
2018-08-21 15:26:40 +0200

    Fix zshrc name in file list

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 dir=`pwd`
 old_dir=$dir/backup
-files="zsh vimrc vim tmux.conf"		#list of files and folders to symlink in HOME
+files="zshrc vimrc vim tmux.conf"		#list of files and folders to symlink in HOME
 
 ########
 
