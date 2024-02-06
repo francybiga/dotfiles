41b5884e8e158b417e26ab7df973491b076a09cb
Francesco Bigagnoli <fbigagnoli@spotify.com>
2024-02-06 20:17:52 +0100

    [fzf] Do not restore query_subdir and fix comments

--- fzf.zsh
+++ fzf.zsh
@@ -16,15 +16,16 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 # ------------
 
 # Bazel query (bzq alias)
-#   e.g. bzq **<TAB> or bzq DIRECTORY **<TAB> to show only targets of a given subdir
-_fzf_complete_bzq() {
+#   e.g. bzb **<TAB> or bzb DIRECTORY **<TAB> to show only targets of a given subdir
+_fzf_complete_bzb() {
     # Split the arguments into an array and assign the second word as query_subdir to filter on that specific subdir
     local -a args
     IFS=' ' read -A args <<< "$@"
-    # Note that this assumes the usage of bzq and not "bazel query", since it consider the 2nd argument to be the subdir
+    # Note that this assumes the usage of bzb and not "bazel build", since it consider the 2nd argument to be the subdir
     query_subdir="${args[2]}"
 
-   _fzf_complete --multi --reverse -- "$@" < <(\
+   # Note that we do not restore the entire args but we drop the query_subdir (will be substituted by the fzf selection)
+   _fzf_complete --multi --reverse -- "${args[1]} " < <(\
     bzq_cmd="bazel query ${query_subdir}/... --keep_going --noshow_progress"
     eval "$bzq_cmd"
   )
